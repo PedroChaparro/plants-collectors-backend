9dacf015c56fec04e542535e0be451dfaaf4e76e
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-03-02 13:33:36 -0500

    feat(plants): ✨ Insert images and resize images
    Insert planst to database using random users id and resize the initial images to normalize the size.

--- src/index.database.ts
+++ src/index.database.ts
@@ -1,12 +1,18 @@
 import { faker } from "@faker-js/faker";
 import { DatabasePool } from "./database/database.js";
 import { hashPassword } from "./lib/bcrypt.lib.js";
+import path from "path";
+import sharp from "sharp";
+import plants from "./database/plants.json" assert { type: "json" };
 
 // Global variables
+const IMAGES_PATH = path.join(process.cwd(), "src", "public", "imgs");
 const usersIds: number[] = [];
 
-// Genereta en insert random users
+// Generete and insert random users
 const insertRandomUsers = async () => {
+  console.log("Inserting random users...");
+
   try {
     // Create an array of 10 users
     const users = Array.from({ length: 10 }, (_, i) => ({
@@ -37,12 +43,71 @@ const insertRandomUsers = async () => {
     );
   } catch (error) {
     console.log("Error: ", error);
+    process.exit(1);
   }
+
+  console.log("Users inserted!");
+};
+
+// Normalize the plants images to 500x500
+const normalizePlantsImages = async () => {
+  console.log("Normalizing plants images...");
+
+  Promise.all(
+    plants.map(async (plant) => {
+      try {
+        // Files should be named as the plant name plus _init.jpg
+        const inputFileName = plant.toLowerCase().split(" ").join("_");
+        const inputFilePath = path.join(
+          IMAGES_PATH,
+          inputFileName + "_init" + ".jpg"
+        );
+
+        // The output file should be named as the plant name plus .jpg
+        // TODO: Use the plant id instead of the name
+        const outputFilePath = path.join(IMAGES_PATH, inputFileName + ".jpg");
+
+        // Resize and save the image
+        await sharp(inputFilePath)
+          .resize({ width: 500, height: 500, fit: "cover" })
+          .toFile(outputFilePath);
+      } catch (error) {
+        console.log("Error: ", error);
+        process.exit(1);
+      }
+    })
+  );
+
+  console.log("Images normalized!");
+};
+
+// Insert the plants
+const insertPlants = async () => {
+  console.log("Inserting plants...");
+
+  try {
+    // Insert the plants at once
+    await Promise.all(
+      plants.map(async (plant) => {
+        const randomUserIndex = Math.floor(Math.random() * usersIds.length);
+        const sellerId = usersIds[randomUserIndex];
+        const query = "INSERT INTO plants (name, seller_id) VALUES ($1, $2)";
+        DatabasePool.query(query, [plant, sellerId]);
+      })
+    );
+  } catch (error) {
+    console.log("Error: ", error);
+    process.exit(1);
+  }
+
+  console.log("Plants inserted!");
 };
 
 // Execute the functions
 const main = async () => {
   await insertRandomUsers();
+  await normalizePlantsImages();
+  await insertPlants();
 };
 
 main();
